7e9bf18ed0a413e13cdda49dd19165824bb08711
Pranav <95164530+thefakepranav@users.noreply.github.com>
2022-10-04 16:05:07 +0530

    renamed navbar as header

--- backend/backend/src/index.js
+++ backend/backend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/Navbar/App";
+import App from "./components/Header/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
