ba8318dd8faef5fc8d77cc88ef4f261913552f26
Pranav <95164530+thefakepranav@users.noreply.github.com>
2022-10-27 23:42:22 +0530

    added carousel

--- frontend/src/screens/Home/HomeScreen.js
+++ frontend/src/screens/Home/HomeScreen.js
@@ -1,9 +1,19 @@
 import React from "react";
+import img1 from "./image1.png";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
 
 const HomeScreen = () => {
   return (
     <>
-    Home
+      <Splide aria-label="My Favorite Images">
+        <SplideSlide>
+          <img className="w-full" src={img1} alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img className="w-full" src={img1} alt="Image 2" />
+        </SplideSlide>
+      </Splide>
     </>
   );
 };
