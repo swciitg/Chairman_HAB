d662c3a2c24e816a612865c5d11ac4675a4bafef
Geet-manik <106328996+Geet-manik@users.noreply.github.com>
2022-12-10 22:31:16 +0530

    .final

--- frontend/src/components/Members/Group-member-card.jsx
+++ frontend/src/components/Members/Group-member-card.jsx
@@ -1,49 +1,49 @@
-import styles from "./Member.modules.css";
-import Pic from'./pic.png';
-const Group_member_cards = () =>  {
+import styles from "./Members.module.css";
+import Pic from './pic.png';
+const Groupmembercards = () =>  {
     
     return (
-        <div className={`flex items-center justify-center ${styles.Members-cards} `}>
-        <div className={` ${styles.Members-cards-img} flex justify-center items-center w-1/3`}>
- <img scr={Pic} alt="" />
-        </div>
-        <div className={`${styles.Members-cards-content}  flex  flex-col `}>
-        
-             <div className={` ${styles.Research-Group-details-1 }`}>                Mr. Rakesh Sarkar
-             </div>
-             <br/>
-             <div className={` ${styles.Research-Group-details-2}`}>Department of Physics</div>
-             <div className={` ${styles.Research-Group-details-3}`}>
-                 
-                 
- 
-                 
+        <div className={`flex items-center justify-center  ${styles.members_cards} `}>
+       <div className={` ${styles.members_cardsimg}  flex justify-center items-center w-1/3`}>
+<img scr={Pic} alt=""/>
+       </div>
+       <div className={` ${styles.members_cardscontent}  flex  flex-col `}>
+       
+            <div className={` ${styles.research_groupdetails1 }`}>                Mr. Rakesh Sarkar
+            </div>
+            <br/>
+            <div className={` ${styles.research_groupdetails2}`}>Department of Physics</div>
+            <div className={` ${styles.research_groupdetails3}`}>
+                
+                
+
+                
+          
+                
            
-                 
-            
+               </div>
+
+                <div className={` ${styles.research_groupdetails3}`}> PhD student</div>
+                <div className={` ${styles.research_groupdetails3}`}>
+                    Research interest:Terahertz metamaterial,polarization rotation devices,
+                    Electromagnetically induced transparency and toroidal metamaterial.
                 </div>
- 
-                 <div className={` ${styles.Research-Group-details-3}`}> PhD student</div>
-                 <div className={` ${styles.Research-Group-details-3}`}>
-                     Research interest:Terahertz metamaterial,polarization rotation devices,
-                     Electromagnetically induced transparency and toroidal metamaterial.
-                 </div>
-                 <div className={` ${styles.Research-Group-details-3}`}>
-                     Google Scholar: Rakesh_GS
- 
-                 </div>
-                 <div className={` ${styles.Research-Group-details-3}`}>
-                     Email: rakeshphys163@gmail.com
- 
- 
-                 </div>  <div className={` ${styles.Research-Group-details-3}`}>
-                     Phone: +91 9382011054 (M) 
- 
-                 </div>
-        </div>
-     
+                <div className={` ${styles.research_groupdetails3}`}>
+                    Google Scholar: Rakesh_GS
+
+                </div>
+                <div className={` ${styles.research_groupdetails3}`}>
+                    Email: rakeshphys163@gmail.com
+
+
+                </div>  <div className={` ${styles.research_groupdetails3}`}>
+                    Phone: +91 9382011054 (M) 
+
+                </div>
+       </div>
     
-     </div>
+   
+    </div>
         );
     }
-export default  Group_member_cards;
+export default  Groupmembercards;
