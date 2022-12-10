fbfb96abd316efebdc04272626d796b526c10576
Geet-manik <106328996+Geet-manik@users.noreply.github.com>
2022-12-10 22:34:21 +0530

    final

--- frontend/src/components/Members/Alumni-card.jsx
+++ frontend/src/components/Members/Alumni-card.jsx
@@ -1,26 +1,25 @@
-// import styles from "./Member.modules.css";
-// import Pic from'./pic.png';
-// const Alumni_cards = () =>  {
+import styles from "./Members.module.css";
+import Pic from'./pic.png';
+const Alumnicards = () =>  {
     
-//     return (
-
-//         <div className={`flex items-center justify-center   ${styles.Alumni-cards}`}>
-//         <div className={` ${styles.Members-cards-img} flex justify-center items-center w-1/3`}>
-//  <img scr={Pic} alt=""/>
-//         </div>
-//         <div className={`${styles.Members-cards-content}  flex  flex-col `}>
+    return (
+        <div className={`flex items-center justify-center   ${styles.alumni_cards}`}>
+        <div className={` ${styles.members_cardsimg} flex justify-center items-center w-1/3`}>
+          <img scr={Pic} alt=""/>
+        </div>
+        <div className={`${styles.members_cardscontent}  flex  flex-col `}>
         
-//              <div className={` ${styles.Research-Group-details-1 }`}>        Mr. Maidul Islam
+             <div className={` ${styles.research_groupdetails1 }`}>        Mr. Maidul Islam
                  
  
  
  
  
  
-//              </div>
-//              <br/>
-//              <div className={` ${styles.Research-Group-details-2}`}>Assistant professor</div>
-//              <div className={` ${styles.Research-Group-details-3}`}>
+             </div>
+             <br/>
+             <div className={` ${styles.research_groupdetails2}`}>Assistant professor</div>
+             <div className={` ${styles.research_groupdetails3}`}>
                  
                  
  
@@ -28,25 +27,26 @@
            
                  
             
-//                 </div>
+                </div>
  
-//                  <div className={` ${styles.Research-Group-details-3}`}> Year of completion: 2018</div>
-//                  <div className={` ${styles.Research-Group-details-3}`}>
-//                      The Assam Royal Global University
-//                  </div>
-//                  <div className={` ${styles.Research-Group-details-3}`}>
-//                      Email: maidul.iitg@gmail.com
+                 <div className={` ${styles.research_groupdetails3}`}> Year of completion: 2018</div>
+                 <div className={` ${styles.research_groupdetails3}`}>
+                     The Assam Royal Global University
+                 </div>
+                 <div className={` ${styles.research_groupdetails3}`}>
+                     Email: maidul.iitg@gmail.com
  
  
-//                  </div>
-//                  <div className={` ${styles.Research-Group-details-3}`}>
-//                      Phone: +91 (M)
+                 </div>
+                 <div className={` ${styles.research_groupdetails3}`}>
+                     Phone: +91 (M)
  
-//                  </div> 
-//         </div>
+                 </div> 
+        </div>
      
     
-//      </div>
-//         );
-//     }
-// export default Alumni_cards;
+     </div>
+ 
+        );
+    }
+export default Alumnicards;
