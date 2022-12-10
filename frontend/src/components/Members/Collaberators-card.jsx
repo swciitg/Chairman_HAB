2d0bfec4df82dc2179fefa6662bd8e2cb12bd4d9
Geet-manik <106328996+Geet-manik@users.noreply.github.com>
2022-12-10 20:40:05 +0530

    ...

--- frontend/src/components/Members/Collaberators-card.jsx
+++ frontend/src/components/Members/Collaberators-card.jsx
@@ -1,7 +1,15 @@
 import styles from "./Members.module.css";
+import axios from "axios";
 
 const Collaberatorscards = () =>  {
-    
+  
+
+    axios
+    .get("http://localhost:5000/chairman_hab/api")
+    .then(data => console.log(data.data))
+    .catch(error => console.log(error));
+   
+ 
     return (
 
         <div className={`flex flex-col justify-center ${styles.collaberators_cards}`}>
