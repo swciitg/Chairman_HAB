94642abd0288ce8e0b9bfacdcbd1484b81f39391
Yashi Natu <yashinatu2007@gmail.com>
2022-12-10 19:25:02 +0530

    admin screen url resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,37 +3,16 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { BASEURL } from "./constant";
 import AdminScreen from "./screens/admin/AdminScreen";
-import HomeScreen from "./screens/Home/HomeScreen";
-import MembersScreen from "./screens/Members/MembersScreen";
-import ProjectsScreen from "./screens/Projects/ProjectsScreen";
-import PublicationsScreen from "./screens/Publications/PublicationsScreen";
-import ResearchScreen from "./screens/Research/ResearchScreen";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import  Table  from "./components/Project-Table/Table";
+import Main from "./screens/Main/Main";
 function App() {
   return (
     <>
-      {/* <div>
-        <Link to="/">Home</Link>
-        <Link to="/research">Research</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/publications">Publications</Link>
-        <Link to="/members">Members</Link>
-      </div> */}
-
       <BrowserRouter basename={BASEURL}>
-        <Header />
-        <Routes>
-          <Route path="/admin/*" exact element={<AdminScreen />} />
-          <Route path="/*" exact element={<HomeScreen />} />
-          <Route path="/research/*" exact element={<ResearchScreen />} />
-          <Route path="/projects/*" exact element={<ProjectsScreen />} />
-          <Route path="/publications/*" exact element={<PublicationsScreen />} />
-          <Route path="/members/*" exact element={<MembersScreen />} />
+        <Routes >
+        <Route path="/*" exact element={<Main />} />
+        <Route path="/admin/*" exact element={<AdminScreen />} />
         </Routes>
       </BrowserRouter>
-        <Footer />
       <Toaster />
     </>
   );
