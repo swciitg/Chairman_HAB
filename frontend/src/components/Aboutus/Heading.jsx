f0bc27c36e7657fd62fbc08c2084186d6949e79a
Geet-manik <106328996+Geet-manik@users.noreply.github.com>
2022-12-10 22:37:55 +0530

    final

---
+++ frontend/src/components/Aboutus/Heading.jsx
@@ -0,0 +1,15 @@
+import styles from "./About.module.css"
+
+
+const Headings = () =>  {
+    
+    return (
+
+        <div className={`${styles.headings}`}>
+        <span className={`${styles.dark}`}> Best Paper Award </span> for "Planar Plasmonic Terahertz Waveguides for Sensor Applications" in ICOLS 2018 held in Zurich on Jan. 15-16, 2018.geet
+     
+         
+     </div>
+        );
+    }
+export default Headings;
