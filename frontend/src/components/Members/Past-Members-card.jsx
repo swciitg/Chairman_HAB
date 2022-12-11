db15e29ee42ca68eb9228826ee416fbacf185cf5
Geet-manik <106328996+Geet-manik@users.noreply.github.com>
2022-12-11 20:00:33 +0530

    .final

--- frontend/src/components/Members/Past-Members-card.jsx
+++ frontend/src/components/Members/Past-Members-card.jsx
@@ -1,6 +1,19 @@
 
 import styles from "./Members.module.css";
+import React, { useState } from "react";
+
+import axios from "axios";
+
 const PastMembers = () => {
+  const [notes, setNotes] = useState([]);
+  const url = "http://localhost:5000/api/PastMembers";
+  const promise = axios.get(url);
+  promise.then((res) => {
+    const data = res.data.data.PastMember;
+    setNotes(data);
+    // console.log(data);
+  });
+
   return (
     <div className={`flex items-center  ${styles.pastmembers_cards}`}>
 <b> Shivam Rajguru</b> (B.tech project student, 2020-2021)
