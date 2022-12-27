0191e63bb7ac1e5b1726dea7257b2b2ae7c92c7d
Pranav <95164530+thefakepranav@users.noreply.github.com>
2022-12-27 18:43:38 +0530

    keyReasearchArea post integrated

--- backend/models/keyReasearchArea.js
+++ backend/models/keyReasearchArea.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const keyResearchAreaSchema = new Schema({
-    keyResearchAreaTitle: { type: String, required: true },
-    keyResearchAreaDescription: {type: String, required: true },
-    // keyResearchAreaImage: { data: Buffer, contentType: String }
-    keyResearchAreaImage : {type: String}
+  keyResearchAreaTitle: { type: String, required: true },
+  keyResearchAreaDescription: { type: String, required: true },
+  // keyResearchAreaImage: { data: Buffer, contentType: String }
+  keyResearchAreaImage: { type: String },
 });
-const KeyResearchArea = mongoose.model('keyResearchArea', keyResearchAreaSchema);
+const KeyResearchArea = mongoose.model(
+  "keyResearchArea",
+  keyResearchAreaSchema
+);
 module.exports = KeyResearchArea;
