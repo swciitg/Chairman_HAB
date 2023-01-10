d5882b776b2d30ad939be20407bc71f1df3b475c
yashinatu <yashinatu2007@gmail.com>
2023-01-10 19:17:46 +0530

    admin

--- frontend/src/screens/admin/PastMembers/PastMembers.js
+++ frontend/src/screens/admin/PastMembers/PastMembers.js
@@ -25,7 +25,7 @@ const PastMembersScreen = () => {
   const pastMembersDelete = (id) => {
     axios
       .delete(`${BACKEND_API}/pastMembers/${id}`)
-      .then((res) => console.log(res))
+      .then((res) => window.location.reload())
       .catch((err) => console.log(err));
   };
     return (
--- frontend/src/screens/admin/Profile/Profile.js
+++ frontend/src/screens/admin/Profile/Profile.js
@@ -25,7 +25,7 @@ const ProfileScreen = () => {
   const profileDelete = (id) => {
     axios
       .delete(`${BACKEND_API}/profile/${id}`)
-      .then((res) => console.log(res))
+      .then((res) => window.location.reload())
       .catch((err) => console.log(err));
   };
     return (
